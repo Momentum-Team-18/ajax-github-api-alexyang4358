0f4c4f485e5c9c44b1cd402534be0ee43edfbd73
alexyang4358 <alexyang4358@gmail.com>
2023-04-27 17:19:55 -0400

    basic layout

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 // location, url, username, repos, avatar image
 // create a for loop to gather attributes
 
-
 let mainContainer = document.querySelector("#mainContainer");
+let header = document.querySelector("#header");
+let subHeader = document.querySelector("#subHeader");
+let repoContainer = document.querySelector("#repoContainer")
 let URL = "https://api.github.com/users/alexyang4358";
 
 fetch(URL, {
@@ -14,32 +16,45 @@ fetch(URL, {
     .then((response) => {
         console.log(response)
         return response.json();
-        //meta data of alexyang4358 
+        //meta data of alexyang4358
 })
     .then((body) => {
         console.log(body);
         return body;
         //attributes from alexyang4358 ready
 })
-    .then((attributes) => {
-    let name = document.createElement("h3");
+
+.then((attributes) => {
+    
+    let imageURL = document.createElement("img");
+        imageURL.src = `${attributes.avatar_url}`;
+        header.appendChild(imageURL);
+        imageURL.classList.add("profileImage")
+    
+    let name = document.createElement("p");
         name.innerText = `${attributes.name}`;
-        mainContainer.appendChild(name);
-    let location = document.createElement("h3");
-        location.innerText = `${attributes.location}`;
-        mainContainer.appendChild(location);
-    let login = document.createElement("h3");
-        login.innerText = `${attributes.login}`;
-        mainContainer.appendChild(login);
-    let webURL = document.createElement("h3");
-        webURL.innerText = `${attributes.html_url}`;
-        mainContainer.appendChild(webURL);
+        header.appendChild(name);
+        name.classList.add("header")
+
+    let location = document.createElement("p");
+        location.innerText = `Location: ${attributes.location}`;
+        subHeader.appendChild(location);
+        location.classList.add("subHeader")
+
+    let login = document.createElement("p");
+        login.innerText = `GitHub username: ${attributes.login}`;
+        subHeader.appendChild(login);
+        login.classList.add("subHeader");
+
+    let webURL = document.createElement("p");
+        webURL.innerText = `GitHub URL: ${attributes.html_url}`;
+        subHeader.appendChild(webURL);
+        webURL.classList.add("subHeader");
+
     let repoURL = document.createElement("h3");
         repoURL.innerText = `${attributes.repos_url}`;
-        mainContainer.appendChild(repoURL);
-    let imageURL = document.createElement("h3");
-        imageURL.innerText = `${attributes.avatar_url}`;
-        mainContainer.appendChild(imageURL);
+        repoContainer.appendChild(repoURL);
+        repoURL.classList.add("repoContainer");
 });
 
 //
