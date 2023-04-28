402fa6796905ce052a1d8bb1353bc912a1c021b4
alexyang4358 <alexyang4358@gmail.com>
2023-04-28 12:20:55 -0400

    trouble with links

--- main.js
+++ main.js
@@ -5,8 +5,10 @@
 let mainContainer = document.querySelector("#mainContainer");
 let header = document.querySelector("#header");
 let subHeader = document.querySelector("#subHeader");
-let repoContainer = document.querySelector("#repoContainer")
+let repoContainer = document.querySelector("#repoContainer");
+let repoTitle =document.querySelector("#repoTitle");
 let URL = "https://api.github.com/users/alexyang4358";
+let rURL = "https://api.github.com/users/alexyang4358/repos";
 
 fetch(URL, {
     method: "GET",
@@ -51,14 +53,54 @@ fetch(URL, {
         subHeader.appendChild(webURL);
         webURL.classList.add("subHeader");
 
-    let repoURL = document.createElement("h3");
-        repoURL.innerText = `${attributes.repos_url}`;
-        repoContainer.appendChild(repoURL);
-        repoURL.classList.add("repoContainer");
+    // let repoURL = document.createElement("a");
+    //     repoURL.innerText = `${attributes.repos_url}`;
+    //     repoContainer.appendChild(repoURL);
+    //     repoURL.classList.add("repoContainer");
+    //     console.log(repoURL);
 });
 
-//
+fetch(rURL, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+})
 
+.then((response) => {
+    console.log(response)
+    return response.json();
+
+})
+
+.then((body) => {
+    console.log(body);
+    return body;
+})
+
+.then((parsedRepos) => {
+    for (let repos of parsedRepos) {
+    console.log(repos.html_url);
+        let reposEl = document.createElement("a");
+        reposEl.href = repos.html_url;
+        repoContainer.appendChild(reposEl);
+    }
+});
+
+
+// .then((parsedRepos) => {
+//     console.log(parsedRepos)
+//     for (let repos of parsedRepos) {
+//     console.log(repos.url);
+//         let reposEl = document.createElement ("p");
+//         reposEl.innerText = repos.url;
+//         repoContainer.appendChild(reposEl);
+//     }
+// });
+
+
+
+
+
+// how to add other projects to repo //
 
 // console.log(attributes.location);
 // console.log(attributes.name);
@@ -66,19 +108,3 @@ fetch(URL, {
 // console.log(attributes.repos_url);
 // console.log(attributes.html_url);
 // console.log(attributes.avatar_url);
-
-
-
-
-
-
-// .then((attributes) => {
-//     for (let details of attributes.location) {
-//         console.log(details.location);
-//         let name = document.createElement("h3");
-//         name.innerText = `${attributes.location}`;
-//         mainContainer.appendChild(name);
-//     }
-// })
-
-
